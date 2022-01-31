26893d2e6c6215a54d66e5f5937b0e9069bebcbc
geeeyeon <parafunction@hanmail.net>
2022-01-31 23:53:21 +0900

    추가: tailwind 설정

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { authService } from "./fBase";
+import "./index.css";
 
 console.log(authService);
 
